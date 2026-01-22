b3987d9a5aa3635a5adb1eae0847f757b5c2c33f
Mansi566 <mansitejani1110@gmail.com>
2026-01-22 11:35:47 -0500

    port type is changed

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const dbURI = process.env.MONGO_URI || "mongodb://localhost:27017/my_database";
 const app = express();
 app.use(cors()); // This allows your React app to talk to the server
 app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
 // 1. Connect to Local MongoDB
 mongoose
@@ -167,4 +168,8 @@ app.delete("/api/category/:id", async (req, res) => {
   res.json(deleteCategory);
 });
 
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+// app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
