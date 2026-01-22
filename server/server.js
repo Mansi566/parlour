2d810baf96de08e1fe2e43324ae7f30527174533
Mansi566 <mansitejani1110@gmail.com>
2026-01-22 11:31:14 -0500

    server change the code

--- server/server.js
+++ server/server.js
@@ -26,6 +26,11 @@ const upload = multer({ storage });
 // Create the 'uploads' folder statically so you can see the images in browser
 app.use("/uploads", express.static("uploads"));
 
+app.get('/', (req, res) => {
+  res.send('Server is running!'); 
+});
+
+
 // //3. your post route for customer table
 app.post("/api/customers", async (req, res) => {
   try {
